5868bed026d52a9395f23ac098288f59804a0b80
Edwardes Junior Lima Gomes <edwardesjr.limagomes@gmail.com>
2018-07-04 03:42:49 -0300

    Novos comandos SQL

--- comandosSQL.sql
+++ comandosSQL.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+﻿-- phpMyAdmin SQL Dump
 -- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 04-Jul-2018 às 04:53
+-- Generation Time: 04-Jul-2018 às 06:42
 -- Versão do servidor: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -34,10 +34,19 @@ CREATE TABLE IF NOT EXISTS `noticia` (
   `titulo` text NOT NULL,
   `subtitulo` text NOT NULL,
   `autor` text NOT NULL,
-  `conteudo` longtext NOT NULL,
-  `data` date NOT NULL,
+  `conteudo` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `noticia`
+--
+
+INSERT INTO `noticia` (`id`, `titulo`, `subtitulo`, `autor`, `conteudo`) VALUES
+(19, 'Noticia 1', 'Sub1,', 'Autor 1', 'Texto1'),
+(20, 'Noticia 2', 'Sub2,', 'Autor 2', 'Texto 2'),
+(21, 'Noticia 3', 'Sub 3,', 'Autor 3', 'Texto 3'),
+(22, 'Noticia 4', 'Sub 4,', 'Autor 4', 'Texto 4');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
